a37c1ab003b422f4bbb83db211d2bd7660bc064e
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2021-05-20 15:35:46 +0700

    class Diagram

---
+++ Main/DuoLegend/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.Models
+{
+    public class Admin
+    {
+        private string _adminId;
+        private string _adminPassword;
+        private string _userName;
+        private string _dob;
+        private string _email;
+
+        public string AdminId { get; set; }
+        public string AdminPassword { get; set; }
+        public string UserName { get; set; }
+        public string Dob { get; set; }
+        public string Email { get; set; }
+    }
+}
