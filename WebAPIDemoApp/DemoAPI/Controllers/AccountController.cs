ed227a31b5f0dbf396a10b6ca0ee06a9af14129e
hokhaminh <74462162+hokhaminh@users.noreply.github.com>
2021-05-22 22:13:59 +0700

    Project test thử login
    có thể chỉnh sửa hay phá, vì có backup ở máy rồi

---
+++ WebAPIDemoApp/DemoAPI/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoAPI.Models;
+
+namespace DemoAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            conn.ConnectionString = "data source=SK-20190915MKOH; database=test; integrated security = SSPI;";
+        }[HttpPost]
+        public ActionResult checkLogin(user account)
+        {
+            connectionString();
+            conn.Open();
+            com.Connection = conn;
+            com.CommandText = "select * from userAccount where username = '"+account.username+"' and password = '"+account.password+"' "; ;
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                conn.Close();
+                return View("mainpage");
+            }
+            else
+            {
+                conn.Close();
+                return View("error");
+            }
+            
+
+        }
+    }
+}
---
+++ WebAPIDemoApp/DemoAPI/Models/user.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class user
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
