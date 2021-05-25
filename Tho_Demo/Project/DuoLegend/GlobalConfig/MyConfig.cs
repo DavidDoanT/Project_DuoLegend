cf1d2fb959039a5746c0492f9400e965aa299350
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2021-05-26 06:02:20 +0700

    .

---
+++ Tho_Demo/Project/DuoLegend/GlobalConfig/MyConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.GlobalConfig
+{
+    public static class MyConfig
+    {
+        public static string ConnectionString { get;} = "Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.0.2;Integrated Security=True";
+    }
+}
