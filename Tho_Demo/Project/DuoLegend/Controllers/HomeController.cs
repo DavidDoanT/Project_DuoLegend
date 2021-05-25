08a2fe93723f1298e7ce6788dbce2c8ed45104be
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2021-05-25 09:57:02 +0700

    .

--- Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
+++ Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using DuoLegend.Models;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -26,12 +26,6 @@ namespace DuoLegend.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
