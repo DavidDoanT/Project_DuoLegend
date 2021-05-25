5afef34299720bd728d045c505679e161a5544b0
khanhtoanioe <80748497+khanhtoanioe@users.noreply.github.com>
2021-05-25 16:40:11 +0700

    n

--- Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
+++ Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace DuoLegend.Controllers
         {
             SqlConnection conn = new SqlConnection();
             SqlCommand com = new SqlCommand();
-            conn.ConnectionString = "Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.0.2;Integrated Security=True";
+            conn.ConnectionString = "Data Source=DESKTOP-B97EA2J;Initial Catalog=no_data_DuoDatabase_version-1.0.2;Integrated Security=True";
             
             conn.Open();
             com.Connection = conn;
