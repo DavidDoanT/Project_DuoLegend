5545e9482ea6cd12b2049782c6fd00cbcbddf3e9
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2021-05-25 11:47:10 +0700

    .

--- Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
+++ Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using DuoLegend.Models;
 namespace DuoLegend.Controllers
 {
     public class HomeController : Controller
@@ -24,13 +24,21 @@ namespace DuoLegend.Controllers
         {
             SqlConnection conn = new SqlConnection();
             SqlCommand com = new SqlCommand();
+            SqlDataReader dr;
             conn.ConnectionString = "Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.0.2;Integrated Security=True";
             conn.Open();
+            com.CommandText = "Select inGameName from User LIMIT 3";
+            dr = com.ExecuteReader();
+            dr.Read();
+            string test = (string)dr[$"inGameName"];
+            Test t = new Test();
+            t.inGameName = test;
+
             conn.Close();
             //connection.Open();
             //connection.Close();
             //Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.0.2;Integrated Security=True
-            return View();
+            return View(t);
         }
 
         public IActionResult Privacy()
