2f342faf734c90ceb8acd9811df42dadf8a75f18
David150022 <doanngocminh2@gmail.com>
2021-05-27 15:14:42 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/DavidDoanT/Project_DuoLegend into main""
    This reverts commit 4543a2b68f0d97fc1bdcb8d243827922aaca1368.

--- Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
+++ Tho_Demo/Project/DuoLegend/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using DuoLegend.ViewModels;
 using DuoLegend.GlobalConfig;
 using DuoLegend.DAO;
+using DuoLegend.RiotAPI;
+using DuoLegend.Models;
 
 namespace DuoLegend.Controllers
 {
@@ -25,7 +27,39 @@ namespace DuoLegend.Controllers
 
         public IActionResult Index()
         {
-            return View(UserDAO.getRandomInGameName());
+            var infor = UserDAO.getRandomInGameName();
+            for (int i = 0; i < infor.InGameName.Length; i++)
+            {
+                var userInfor = new userInforMainPage();
+                int totalGameWin = 0;
+                int totalKill = 0;
+                int totalDeath = 0;
+                int totalAssist = 0;
+                if(infor.InGameName[i] is null)
+                {
+                    break;
+                }
+                infor.Rank[i] = RiotAPI.RiotAPI.getRankByEncryptedSummonerId(UserDAO.getEncryptedSummonerId(infor.InGameName[i]), "KR"); //hard code server KR
+                string[] listMatch = RiotAPI.RiotAPI.getListMatchIDbyPuuId(UserDAO.getPuuId(infor.InGameName[i]), "ASIA");
+                for (int j = 0; j < listMatch.Length; j++)
+                {
+                    var smallMatchInfor = RiotAPI.RiotAPI.getMatchInfor(listMatch[j],"ASIA", UserDAO.getEncryptedSummonerId(infor.InGameName[i]));
+                    userInfor.champID[j] = smallMatchInfor.ChampId;
+                    if(smallMatchInfor.IsWin)
+                    {
+                        totalGameWin++;
+                    }
+                    totalKill += smallMatchInfor.Kill;
+                    totalDeath += smallMatchInfor.Death;
+                    totalAssist += smallMatchInfor.Assist;                  
+                }
+                userInfor.WinRate = Math.Round(((double)totalGameWin / listMatch.Length) * 100,1);
+                userInfor.Kill = Math.Round((double)totalKill / listMatch.Length,1);
+                userInfor.Death = Math.Round((double)totalDeath / listMatch.Length,1);
+                userInfor.Assist = Math.Round((double)totalAssist / listMatch.Length,1);
+                infor.ListUserInfor[i] = userInfor;
+            }
+            return View(infor);
         }
 
         public IActionResult Privacy()
---
+++ Tho_Demo/Project/DuoLegend/Models/userInforMainPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.Models
+{
+    public class userInforMainPage
+    {
+        public userInforMainPage()
+        {
+            champID = new int[3];
+        }
+        public double Kill { get; set; }
+        public double Death { get; set; }
+
+        public double Assist { get; set; }
+        public double WinRate { get; set; }
+        public int[] champID { get; set; }
+    }
+}
