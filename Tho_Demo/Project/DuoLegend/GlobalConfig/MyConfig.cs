6f5030d04f9d368c3dc733290b4c5ac007a2af54
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2021-06-04 14:41:44 +0700

    .

--- Tho_Demo/Project/DuoLegend/GlobalConfig/MyConfig.cs
+++ Tho_Demo/Project/DuoLegend/GlobalConfig/MyConfig.cs
@@ -8,6 +8,6 @@ namespace DuoLegend.GlobalConfig
     public static class MyConfig
     {
         public static string ConnectionString { get;} = "Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.0.4;Integrated Security=True";
-        public static string RiotKey { get; set; } = "RGAPI-e4d51340-7eab-4ae2-9f8b-3d9a056d49cb";
+        public static string RiotKey { get; set; } = "RGAPI-1ade78ac-a014-4f9e-a80c-ff0a2b82811b";
     }
 }
