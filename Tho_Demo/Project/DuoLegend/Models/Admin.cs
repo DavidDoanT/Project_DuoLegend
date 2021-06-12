51a90341d88f44027059cae5e39650049576bc71
David150022 <doanngocminh2@gmail.com>
2021-06-12 17:14:56 +0700

    Added validation for admin login

--- Tho_Demo/Project/DuoLegend/Models/Admin.cs
+++ Tho_Demo/Project/DuoLegend/Models/Admin.cs
@@ -1,5 +1,7 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace DuoLegend.Models
 {
     public class Admin
@@ -10,8 +12,10 @@ namespace DuoLegend.Models
         private string _email;
 
         public int AdminId { get => _adminId; set => _adminId = value; }
+        [Required(ErrorMessage = "Please enter your password")]
         public string AdminPassword { get => _adminPassword; set => _adminPassword = value; }
         public string AdminName { get => _adminName; set => _adminName = value; }
+        [Required(ErrorMessage = "Please enter your email")]
         public string Email { get => _email; set => _email = value; }
     }
 }
