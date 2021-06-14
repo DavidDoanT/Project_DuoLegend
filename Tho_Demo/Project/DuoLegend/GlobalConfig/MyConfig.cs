9a2acca012f13175ecd221fc2ddcd104f20a8d71
hokhaminh <74462162+hokhaminh@users.noreply.github.com>
2021-06-14 15:41:26 +0700

    Revert "Tự tạo lại file MyConfig.cs nhe, code của file ở dưới phần mô tả"
    This reverts commit 4a7feb58f11660c29779917551f4d34416372770.

---
+++ Tho_Demo/Project/DuoLegend/GlobalConfig/MyConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.GlobalConfig
+{
+    public static class MyConfig
+    {
+        public static string ConnectionString { get;} = "Data Source=ADMIN;Initial Catalog=no_data_DuoDatabase_version-1.2.0;Integrated Security=True";
+        public static string RiotKey { get; set; } = "RGAPI-6d869d2d-14a5-4258-bec1-64ef62dfd1ef";
+        
+    }
+}
