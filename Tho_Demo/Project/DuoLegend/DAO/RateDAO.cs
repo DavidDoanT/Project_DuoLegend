242701edc53cba51164d4344ded53a1d2951fbaa
khanhtoanioe <80748497+khanhtoanioe@users.noreply.github.com>
2021-06-14 14:29:43 +0700

    f

---
+++ Tho_Demo/Project/DuoLegend/DAO/RateDAO.cs
@@ -0,0 +1,25 @@
+﻿using DuoLegend.GlobalConfig;
+using DuoLegend.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.DAO
+{
+    public class RatingDAO
+    {
+        private static SqlConnection conn = new SqlConnection();
+        private static SqlCommand com = new SqlCommand();
+        public static void addRating(Rating r)
+        {
+            com.Parameters.Clear();
+            conn.ConnectionString = MyConfig.ConnectionString;
+            conn.Open();
+            com.Connection = conn;
+            com.CommandText = "INSERT INTO Rating(raterId,userId,skillScore,behaviorScore) VALUES(@raterId,@userId,@skillScore,@behaviorScore)";
+            com.Parameters.AddWithValue("@raterId", r.RaterId);
+        }
+    }
+}
---
+++ Tho_Demo/Project/DuoLegend/Models/Rating.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuoLegend.Models
+{
+    public class Rating
+    {
+        // private string _raterId,_userId;
+        // private int _skillScore, _behaviorScore;
+        private string _raterId;
+        public string RaterId
+        {
+            get { return _raterId; }
+            set { _raterId = value; }
+        }
+        private string _userId;
+        public string UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+        private int _skillScore;
+        public int SkillScore
+        {
+            get { return _skillScore; }
+            set { _skillScore = value; }
+        }
+        private int _behaviorScore;
+        public int BehaviorScore
+        {
+            get { return _behaviorScore; }
+            set { _behaviorScore = value; }
+        }
+        
+    }
+}
