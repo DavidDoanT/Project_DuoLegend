5040fe034696c640e433f0d338cfb33b06a08a9f
David150022 <doanngocminh2@gmail.com>
2021-06-27 16:00:27 +0700

    Modification in progress

---
+++ Tho_Demo/Project/DuoLegend/DAO/AdminDAO/AdminManagementDAO.cs
@@ -0,0 +1,33 @@
+using System;
+using static DuoLegend.DatabaseConnection.DbConnection;
+
+namespace DuoLegend.DAO.AdminDAO
+{
+    public class AdminManagementDAO
+    {
+        /// <summary>
+        /// Insert into the database information about a banned user
+        /// </summary>
+        /// <param name="userId">The user's unique identifier to be banned</param>
+        /// <param name="adminId">The id of the admin performing the ban action</param>
+        /// <param name="expirationDate">The expiration date of the ban</param>
+        /// <param name="reason">The reason for the ban</param>
+        /// <returns>An integer indicating if the insert was successful. (1 = success, 0 = fail)</returns>
+        public static int BanUser(int userId, int adminId, DateTime expirationDate, string reason)
+        {
+            Connect();
+
+            Cmd.CommandText = "INSERT INTO [BannedUser] (userId, adminId, expirationDate, reason) "
+                                + "Values (@userId, @adminId, @expirationDate, @reason";
+
+            Cmd.Parameters.AddWithValue("userId", userId);
+            Cmd.Parameters.AddWithValue("adminId", adminId);
+            Cmd.Parameters.AddWithValue("expirationDate", expirationDate);
+            Cmd.Parameters.AddWithValue("reason", reason);
+
+            int rowsAffected = Cmd.ExecuteNonQuery();
+
+            return rowsAffected;
+        }
+    }
+}
---
+++ Tho_Demo/Project/DuoLegend/SessionKeys/Keys.cs
@@ -0,0 +1,12 @@
+namespace DuoLegend.SessionKeys
+{
+    /// <summary>
+    /// Provides access to session keys
+    /// </summary>
+    public static class Keys
+    {
+        private const string _adminId = "_AdminId" ;
+
+        public static string AdminId => _adminId;
+    }
+}
